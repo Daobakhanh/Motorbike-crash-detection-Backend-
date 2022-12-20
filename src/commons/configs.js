678ebd83c870c89225c716be7a9f737dfdfab137
Bùi Đức Nhật <nhaths4701@gmail.com>
2022-12-20 23:02:24 +0700

    Config twilio

--- src/commons/configs.js
+++ src/commons/configs.js
@@ -10,4 +10,9 @@ module.exports = {
   MQTT_USERNAME: process.env.MQTT_USERNAME,
   MQTT_PASSWORD: process.env.MQTT_PASSWORD,
   MQTT_TOPIC_PREFIX: process.env.MQTT_TOPIC_PREFIX || '',
+
+  // Twilio
+  TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
+  TWILIO_PHONE_NUMBER: process.env.TWILIO_PHONE_NUMBER,
 };
